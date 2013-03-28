c8b82c3fe49f3262e7d90ed4c581032f07ff9b88
Mark Starkman <mrstarkman@gmail.com>
2013-03-28 17:38:29 -0300

    added surround plugin

--- vim/install_vim_plugins.sh
+++ vim/install_vim_plugins.sh
@@ -5,3 +5,4 @@ git clone https://github.com/Lokaltog/vim-powerline.git ~/.janus/vim-powerline
 git clone https://github.com/tpope/vim-rails.git ~/.janus/vim-rails
 git clone https://github.com/bronson/vim-trailing-whitespace.git ~/.janus/vim-trailing-whitespace
 git clone https://github.com/mattn/zencoding-vim.git ~/.janus/zencoding-vim
+git clone https://github.com/tpope/vim-surround.git ~/.janus/vim-surround
