6fbb5876997a35cb0734be076292e23b83206568
Mark Starkman <mrstarkman@gmail.com>
2013-03-28 17:25:57 -0300

    added zen coding plugin

--- vim/install_vim_plugins.sh
+++ vim/install_vim_plugins.sh
@@ -4,3 +4,4 @@ git clone https://github.com/kchmck/vim-coffee-script.git ~/.janus/vim-coffee-sc
 git clone https://github.com/Lokaltog/vim-powerline.git ~/.janus/vim-powerline
 git clone https://github.com/tpope/vim-rails.git ~/.janus/vim-rails
 git clone https://github.com/bronson/vim-trailing-whitespace.git ~/.janus/vim-trailing-whitespace
+git clone https://github.com/mattn/zencoding-vim.git ~/.janus/zencoding-vim
