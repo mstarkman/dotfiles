87b3ef041670e0d966ac1ed72933cfc6770cf038
Mark <mrstarkman@gmail.com>
2013-03-28 14:18:41 -0400

    fixed some stuff

--- vim/install_vim_plugins.sh
+++ vim/install_vim_plugins.sh
@@ -3,4 +3,4 @@
 git clone https://github.com/kchmck/vim-coffee-script.git ~/.janus/vim-coffee-script
 git clone https://github.com/Lokaltog/vim-powerline.git ~/.janus/vim-powerline
 git clone https://github.com/tpope/vim-rails.git ~/.janus/vim-rails
-
+git clone https://github.com/bronson/vim-trailing-whitespace.git ~/.janus/vim-trailing-whitespace
