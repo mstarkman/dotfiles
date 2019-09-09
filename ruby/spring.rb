8604b05c8fe6cff8e34103c047256e3251d00637
Mark Starkman <mrstarkman@gmail.com>
2019-09-09 08:25:25 -0400

    changes to spring startup

--- ruby/spring.rb
+++ ruby/spring.rb
@@ -1,3 +1,10 @@
 $LOAD_PATH << "#{Gem.dir}/gems/spring-commands-testunit-1.0.1/lib"
 require 'spring-commands-testunit'
 
+
+Spring.after_fork do
+  if ENV['DEBUGGER_STORED_RUBYLIB']
+    starter = ENV['BUNDLER_ORIG_RUBYOPT'][2..-1]
+    load(starter + '.rb')
+  end
+end
